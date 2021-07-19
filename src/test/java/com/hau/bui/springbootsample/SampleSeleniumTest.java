d8d1cc3e4c0e416ed49cd7dff1241f28ba68ba14
hau-kute <buitahau@gmail.com>
2021-07-20 06:21:17 +0700

    Add firefox binrary to jenkins

--- src/test/java/com/hau/bui/springbootsample/SampleSeleniumTest.java
+++ src/test/java/com/hau/bui/springbootsample/SampleSeleniumTest.java
@@ -3,10 +3,16 @@ package com.hau.bui.springbootsample;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class SampleSeleniumTest {
 
@@ -18,10 +24,23 @@ public class SampleSeleniumTest {
 
     public String GECKO_DRIVER_JENKINS = "/var/jenkins_home/geckodriver";
 
+    public String FIREFOX_BIN = "/var/jenkins_home/firefox/firefox.sh";
+
     @BeforeTest
     public void setupDriver() {
+//        FirefoxOptions firefoxOptions = new FirefoxOptions();
+//        firefoxOptions.setBinary(FIREFOX_BIN);
+        System.setProperty("webdriver.firefox.bin", FIREFOX_BIN);
         System.setProperty("webdriver.gecko.driver", GECKO_DRIVER_JENKINS);
-        driver = new FirefoxDriver();
+
+        File pathBinary = new File(FIREFOX_BIN);
+        FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);
+        DesiredCapabilities desired = DesiredCapabilities.firefox();
+        FirefoxOptions options = new FirefoxOptions();
+        desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
+//        WebDriver driver = new FirefoxDriver(options);
+
+        driver = new FirefoxDriver(options);
     }
 
     @Test
